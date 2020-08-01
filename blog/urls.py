ef164fc615fe13f2cd662e0e95038109f453faed
Bayarsaikhan <bayraa.3033@gmail.com>
2020-08-01 11:13:58 +0900

    url

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post', views.post, name='education'), 
-    path('post', views.post, name='social'),  
-    path('post', views.post, name='politic'),  
-    path('post', views.post, name='business'), 
-    path('post', views.post, name='health'),    
+    path('post', views.post, name='posts'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,26 +11,9 @@ def index(request):
     return render(request, 'index.html', context)
 
 def post(request):    
-    posts = [
-        {
-            'title': 'Post1',
-            'text': 'bla bla bla...',
-            'created': '2020-07-19 10:30'
-        },{
-            'title': 'Post2',
-            'text': 'gahahahahahaahah aaaaaaa',
-            'created': '2020-07-10 15:36'
-        },{
-            'title': 'Post3',
-            'text': 'python is best',
-            'created': '2020-07-18 02:01'
-        }
-    ]
+    category_list = Category.objects.all().order_by('name')
+    context = {
+        'categories': category_list
+    }
+    return render(request, 'post.html', context)
 
-    return render(request, 'post.html', { 'data': posts })
-
-def about(request):
-    return render(request, 'about.html')
-
-def comment(request):    
-    return HttpResponse('comment')

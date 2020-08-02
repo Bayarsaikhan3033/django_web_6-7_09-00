2ed47bd68e642e935d983657e7e7079d94ca9a0a
bayarsaikhan3033 <bayraa.3033@gmail.com>
2020-08-02 10:52:10 +0900

    web

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('post', views.post, name='posts'), 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# python manage.py makemigrations
+# python manage.py migrate
